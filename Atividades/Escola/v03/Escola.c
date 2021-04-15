998d38ac02bdcf1762eab652e473d4e9a6739a1b
euritales <58851676+euritales@users.noreply.github.com>
2021-04-14 21:55:03 -0300

    .

--- Atividades/Escola/v03/Escola.c
+++ Atividades/Escola/v03/Escola.c
@@ -26,14 +26,20 @@ typedef struct{
 	char nome[20];
 	int codigo;
 	int semestre;
-	char professor[12];
+	char professor[30];
 	int status;
-} Materia;	
+} Disciplina;	
 
 void funcaocadastrarAluno(Aluno alunos[], int *totalAlunos);
 void funcaoListarAlunos(Aluno alunos[], int totalAlunos);
 void funcaoRemoverAlunos(Aluno alunos[], int totalAlunos);
 void funcaoEditarAlunos(Aluno alunos [], int totalAlunos);
+void funcaocadastrarProfessor(Professor professores[], int *totalProfessores);
+void funcaoListarProfessores(Professor professores[], int totalProfessores);
+void funcaoRemoverProfessores(Professor professores[], int totalProfessores);
+void funcaoDisciplinas (Disciplina disciplinas[], Professor professores[], Aluno alunos[]);
+
+
 int validaNome(char *nome);
 int validaCpf(char *cpf);
 int validarData(char *data);
@@ -43,12 +49,16 @@ int validarSexo(char *sexo);
 int main() {
 	
 	int i = -1;
+	Disciplina disciplinas[MAX];
 	Aluno alunos[MAX];
+	Professor professores[MAX];
 	int qtd_alunos = 0;
+	int qtd_professores = 0;
+	int verificador = -1;
 	
 	while( i != 0 ){
 		
-		printf("\n\nMenu Principal:\n 1 - Cadastrar Alunos\n 2 - Listar Alunos\n 3 - Editar Aluno\n 4 - Excluir Aluno\n");
+		printf("\n\nMenu Principal:\n-----ALUNOS-----\n 1 - Cadastrar Alunos\n 2 - Editar Aluno\n 3 - Excluir Aluno\n \n-----PROFESSORES-----\n 4 - Cadastrar professor \n 5 - Editar professor \n 6 - Excluir professor \n-----DISCIPLINAS-----\n\n 7 - Disciplinas  \n\n-----RELATORIOS----- \n8 - Listas \n  0 - para sair\n");
 		scanf("%d", &i);
 		switch (i)
         {
@@ -56,21 +66,76 @@ int main() {
 	        	funcaocadastrarAluno(alunos, &qtd_alunos);
 			break;
 	        case 2:
-	        	funcaoListarAlunos(alunos, qtd_alunos);
-			break;
-	        case 3:
 	        	funcaoEditarAlunos(alunos, qtd_alunos);
 			break;
-	        case 4:
+	        case 3:
 	           	funcaoRemoverAlunos(alunos, qtd_alunos);
 			break;        
+	        case 4:
+	           	funcaocadastrarProfessor(professores, &qtd_professores);
+			break;        
+	        case 5:
+	           funcaoEditarProfessores(professores, qtd_professores);
+			break;        
+	        case 6:
+				funcaoRemoverProfessores(professores, qtd_professores);
+			break;
+			case 7:
+				printf("\n1 - Cadastrar Disciplinas:\n");
+				scanf("%d", &verificador);
+				
+				if(verificador == 1){
+					funcaoDisciplinas (disciplinas, professores, alunos);
+					verificador = -1;
+				}
+				
+			break;
+			case 8:
+			printf("\n1 - Listar Alunos \n2 - Listar professores \n0 - voltar\n");
+			scanf("%d", &verificador);
+			if(verificador == 1){
+				funcaoListarAlunos(alunos, qtd_alunos);
+				verificador = -1;
+			}else if(verificador == 2){
+				funcaoListarProfessores(professores, qtd_professores);
+				verificador = -1;
+			}
+			break;     		
         }		
 	}
 	
 	return 0;
 }
 
+void funcaoDisciplinas (Disciplina disciplinas[], Professor professores[], Aluno alunos[]){
+	
+	Aluno descricaoA;
+	Professor descricaoP;
+	Disciplina descricaoD;
+	int vald = 1;	
+	char prof[30];
+	int alu = -1;
+	int i = 0;
+	
+	printf("\nDigite o nome da Materia:\n");
+	fflush(stdin);
+	scanf("%[^\n]",descricaoD.nome);
+		
+	printf("\nDigite o semestre Materia:\n");
+	scanf("%d", descricaoD.semestre);
+	
+	printf("\nDigite o codigo do professor:\n");
+	fflush(stdin);
+	scanf("%[^\n]", prof);;
 
+	if(prof == descricaoP.nome){
+		fflush(stdin);
+		scanf("%[^\n]", descricaoD.professor);
+
+	}
+}
+	
+	
 void funcaoEditarAlunos(Aluno alunos [], int totalAlunos){
 	
 	Aluno descricao;
@@ -157,6 +222,92 @@ void funcaoEditarAlunos(Aluno alunos [], int totalAlunos){
 			
 }
 
+void funcaoEditarProfessores(Professor professores [], int totalProfessores){
+	
+	Professor descricao;
+	int vald = 1;
+	int num_matricula;
+	int verificador = 1;
+	int i;
+	
+	printf("\nInsira o numero do professor para ser editado:\n");
+	scanf("%d", &num_matricula);
+	
+	for(i = 0; i < totalProfessores; i++){		
+		
+		if(professores[i].status == 1 && professores[i].matricula == num_matricula ){
+			
+			while( verificador != 0 ){
+				
+				printf("\n\n-----Menu Edicao Aluno-----\n 1 - Editar nome\n 2 - Editar CPF\n 3 - Editar Data\n 4 -Editar Sexo\n 0 -Voltar Menu Principal\n");
+				scanf("%d", &verificador);
+				
+				switch (verificador)
+		        {
+		        	case 0:
+						break; 
+		        	case 1:
+		        		printf("Digite o novo nome do professor:\n");
+						fflush(stdin);
+						scanf("%[^\n]", descricao.nome);
+					
+						vald = validaNome(descricao.nome);
+						
+						if(vald == 0){
+							printf("nome invalido!");
+							break;
+						}
+						strcpy(professores[i].nome, descricao.nome);
+						
+						printf("mudei...");
+		        		break;
+			        case 2:
+			        	printf("Digite o novo CPF do professor:\n");
+						scanf("%s", descricao.cpf);
+						vald = validaCpf(descricao.cpf);
+						
+						if(vald == 0){
+							printf("CPF invalido!");
+							break;
+						}
+						strcpy(professores[i].cpf, descricao.cpf);
+
+						printf("mudei...");
+						break;		
+			        case 3:
+			        	printf("Digite o novo CPF do professor:\n");
+						scanf("%s", descricao.dataNascimento);
+						vald = validarData(descricao.dataNascimento);
+						
+						if(vald == 0){
+							printf("Data invalida!");
+							break;
+						}
+						strcpy(professores[i].dataNascimento, descricao.dataNascimento);
+
+						printf("mudei...");
+						break;	
+			        case 4:
+			        	printf("Digite o novo sexo do professor:\n");
+						scanf("%s", descricao.sexo);
+						vald = validarSexo(descricao.sexo);
+						
+						if(vald == 0){
+							printf("Sexo invalido!");
+							break;
+						}
+						strcpy(professores[i].sexo, descricao.sexo);
+						printf("mudei...");
+						break;	       
+		        }		
+			}	
+				
+		}
+	}
+
+			
+}
+
 void funcaoListarAlunos(Aluno alunos[], int totalAlunos){
 	
 	int i;
@@ -165,6 +316,20 @@ void funcaoListarAlunos(Aluno alunos[], int totalAlunos){
 
 		if(alunos[i].status == 1){
 			printf("\n\n\n|Nome:\n%s\n|Numero Matricula:\n%d\n|CPF:\n%s\n|Sexo:\n%s\n|Data Nascimento:\n%s\n", alunos[i].nome, alunos[i].matricula, alunos[i].cpf, alunos[i].sexo, alunos[i].dataNascimento);
+		}
+		
+	}
+	
+}
+
+void funcaoListarProfessores(Professor professores[], int totalProfessores){
+	
+	int i;
+
+	for(i = 0; i < totalProfessores; i++){
+
+		if(professores[i].status == 1){
+			printf("\n\n\n|Nome:\n%s\n|Numero Matricula:\n%d\n|CPF:\n%s\n|Sexo:\n%s\n|Data Nascimento:\n%s\n", professores[i].nome, professores[i].matricula, professores[i].cpf, professores[i].sexo, professores[i].dataNascimento);
 		}
 		
 	}
@@ -188,7 +353,22 @@ void funcaoRemoverAlunos(Aluno alunos[], int totalAlunos){
 	printf("Apaguei...");
 }
 
-
+void funcaoRemoverProfessores(Professor professores[], int totalProfessores){
+	
+	
+	int i;
+	int num_matricula;
+	
+	printf("\nInsira o Professor a ser deletado(Informe o numero de Matricula):\n");
+	scanf("%d", &num_matricula);
+	for(i = 0; i < totalProfessores; i++){	
+		
+		if(professores[i].status == 1 && professores[i].matricula == num_matricula ){
+			professores[i].status = 0;
+		}	
+	}	
+	printf("Apaguei...");
+}
 
 void funcaocadastrarAluno(Aluno alunos[], int *totalAlunos){	
 
@@ -234,7 +414,7 @@ void funcaocadastrarAluno(Aluno alunos[], int *totalAlunos){
 	vald = validarSexo(descricao.sexo);
 	
 	if(vald == 0){
-		printf("Sexo invalida!");
+		printf("Sexo invalido!");
 	
 		return;
 	}
@@ -252,6 +432,66 @@ void funcaocadastrarAluno(Aluno alunos[], int *totalAlunos){
 	printf("\nAluno Cadastrado\n");
 }
 
+void funcaocadastrarProfessor(Professor professores[], int *totalProfessores){
+	
+	Professor descricao;
+	int vald = 1;
+	int cont = *totalProfessores;
+	
+	
+	
+	printf("Digite o nome do Professor:\n");
+	fflush(stdin);
+	scanf("%[^\n]", descricao.nome);
+	vald = validaNome(descricao.nome);
+	
+	if(vald == 0){
+		printf("nome invalido!");
+		return;
+	}
+	
+	printf("Digite o CPF do professor:\n");
+	scanf("%s", descricao.cpf);
+	vald = validaCpf(descricao.cpf);
+	
+	if(vald == 0){
+		printf("CPF invalido!");
+	
+		return;
+	}
+	
+	printf("Digite a data de nascimento do professor:\n");
+	scanf("%s", descricao.dataNascimento);
+	vald = validarData(descricao.dataNascimento);
+	
+	if(vald == 0){
+		printf("Data invalida!");
+	
+		return;
+	}
+	
+	printf("Digite o sexo do professor:\n");
+	scanf("%s", descricao.sexo);
+	vald = validarSexo(descricao.sexo);
+	
+	if(vald == 0){
+		printf("Sexo invalido!");
+	
+		return;
+	}
+	
+		*totalProfessores += 1;
+	
+	professores[cont].matricula = cont + 1; 
+	strcpy(professores[cont].nome, descricao.nome);
+	strcpy(professores[cont].cpf, descricao.cpf);
+	strcpy(professores[cont].dataNascimento, descricao.dataNascimento);
+	strcpy(professores[cont].sexo, descricao.sexo);
+	professores[cont].status = 1;
+	
+	printf("\n Professor Cadastrado\n");
+
+}
 
 int validaNome(char *nome){
     int contador=0;
@@ -272,7 +512,6 @@ int validaNome(char *nome){
     }
 
 }
-
 
 int validaCpf(char *cpf){
 
